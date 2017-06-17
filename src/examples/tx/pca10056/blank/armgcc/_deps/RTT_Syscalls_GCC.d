db9668f78b55b8a54a5bb63adc429564fd7bebeb
Daniel Veilleux <inductivekickback@gmail.com>
2017-06-17 16:51:39 -0700

    Removing _deps directory.

--- src/examples/tx/pca10056/blank/armgcc/_deps/RTT_Syscalls_GCC.d
+++
@@ -1,2 +0,0 @@
-./_debug/RTT_Syscalls_GCC.o _release/RTT_Syscalls_GCC.o: \
- C:/Users/foolsday/workspace/nRF5_SDK_13.0.0_04a0bfd/external/segger_rtt/RTT_Syscalls_GCC.c
--- src/examples/tx/pca10056/blank/armgcc/_deps/SEGGER_RTT_printf.d
+++
@@ -1,2 +0,0 @@
-./_debug/SEGGER_RTT_printf.o _release/SEGGER_RTT_printf.o: \
- C:/Users/foolsday/workspace/nRF5_SDK_13.0.0_04a0bfd/external/segger_rtt/SEGGER_RTT_printf.c
